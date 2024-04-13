08ea7dba14a9ccee3ba4730893835fd3b6dcbc51
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-04-12 22:52:14 -0300

    Ejercicios

---
+++ EjerciciosUTN/LibreriaClases/Validador.cs
@@ -0,0 +1,14 @@
+﻿namespace LibreriaClases
+{
+    public class Validador
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            if (valor < min || valor > max)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
