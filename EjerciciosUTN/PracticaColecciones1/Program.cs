89c2d972325c8133c1f44f091b34d5b16b730e0f
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-04-30 21:38:29 -0300

    Trivia de coleciiones Queue y stack -

--- EjerciciosUTN/PracticaColecciones1/Program.cs
+++ EjerciciosUTN/PracticaColecciones1/Program.cs
@@ -13,16 +13,26 @@
             for (int i = 0; i <= cantidad * 2; i += 2)
             {
                 cola.Enqueue(i);
+                Console.WriteLine($"(Queue) COLA -- ORDEN DE AGREGADO: {i}");
             }
 
             foreach (int i in cola)
             {
                 pila.Push(i);
+                Console.WriteLine($"(stack)PILA - ORDEN DE AGREGADO: {i}");
             }
+
+            //foreach (int i in cola)
+            //{
+            //    //Console.Write("{0}, ", i);
+            //    Console.WriteLine($"(Queue)COLA - ORDEN DE MOSTRADO: {i}");
+            //}
+
 
             foreach (int i in pila)
             {
-                Console.Write("{0}, ", i);
+                //Console.Write("{0}, ", i);
+                Console.WriteLine($"(stack)PILA - ORDEN DE MOSTRADO: {i}");
             }
         }
     }
