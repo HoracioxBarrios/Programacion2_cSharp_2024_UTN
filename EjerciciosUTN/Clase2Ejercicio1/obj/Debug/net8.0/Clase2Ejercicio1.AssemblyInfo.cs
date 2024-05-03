7c069c43f6e982f4e26f76d4194e7dca5c7c0f52
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-05-03 15:22:41 -0300

    Acomode el codigo

--- EjerciciosUTN/Clase2Ejercicio1/obj/Debug/net8.0/Clase2Ejercicio1.AssemblyInfo.cs
+++ EjerciciosUTN/Clase2Ejercicio1/obj/Debug/net8.0/Clase2Ejercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clase2Ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8c509c6f66cba1f9e7835e0ac870f06a40332a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b027dba1a6cbdbf4606384ea311e29a572a6280")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clase2Ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clase2Ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EjerciciosUTN/LibreriaClases/obj/Debug/net8.0/LibreriaClases.AssemblyInfo.cs
+++ EjerciciosUTN/LibreriaClases/obj/Debug/net8.0/LibreriaClases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaClases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8c509c6f66cba1f9e7835e0ac870f06a40332a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b027dba1a6cbdbf4606384ea311e29a572a6280")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaClases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaClases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
