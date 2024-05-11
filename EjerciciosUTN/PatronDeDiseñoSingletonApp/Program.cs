e6e5f747eba10b32be8dce2acb387df61049fc13
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-05-10 22:57:58 -0300

    Patron de diseño singleton : Que es?

--- EjerciciosUTN/PatronDeDiseñoSingletonApp/Program.cs
+++ EjerciciosUTN/PatronDeDiseñoSingletonApp/Program.cs
@@ -4,6 +4,12 @@ namespace PatronDeDiseñoSingletonApp
 {
     internal class Program
     {
+        /*
+            Singleton es un patrón de diseño creacional que 
+            nos permite asegurarnos de que una clase tenga una única 
+            instancia, a la vez que proporciona un punto de acceso 
+            global a dicha instancia.
+         */
         static void Main(string[] args)
         {
             Usuario user = Usuario.getInstance();
